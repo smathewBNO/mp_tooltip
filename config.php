752352ff604bc3c5457e1ddfe6ab1cf720cba01d
Shawn Matthew <smatthew@bnoinc.com>
2015-07-14 12:58:39 -0400

    add files

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+
+ini_set( 'display_errors', true );
+date_default_timezone_set( 'America/New_York' );
+
+
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 50px;
+                padding-bottom: 20px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+
+      <header id="header" class="header">
+        <h1>
+            <a href="/" title="MasterPass" class="ajaxify" data-href="/">MasterPass</a>
+        </h1>
+        <a class="touch-nav-drawer"></a>
+        <nav id="header-nav" class="header-nav">
+            <ul class="nav-items" style="height: 0px;">
+                <li class="everyone main-nav-item active" data-page="everyone">
+                    <a href="/foreveryone" class="ajaxify" data-href="/foreveryone" title="For Everyone">
+                        <span class="box" data-localize="global.navigation.for">For</span>
+                        <span data-localize="global.navigation.everyone">Everyone</span>
+                    </a>
+                </li>
+                <li class="merchants main-nav-item" data-page="merchants">
+                    <a href="/formerchants" class="ajaxify" data-href="/formerchants" title="For Merchants">
+                        <span class="box" data-localize="global.navigation.for">For</span>
+                        <span data-localize="global.navigation.merchants">Merchants</span>
+                    </a>
+                </li>
+                <li class="partners main-nav-item" data-page="partners">
+                    <a href="/forpartners" class="ajaxify" data-href="/forpartners" title="For Partners">
+                        <span class="box" data-localize="global.navigation.for">For</span>
+                        <span data-localize="global.navigation.partners">Wallet Partners</span>
+                    </a>
+                </li>
+              </ul>
+    </header>
+
+    </body>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    </body>
+</html>
